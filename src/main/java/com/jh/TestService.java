4dbf62e47796981df3783bad32e18d86d0f19595
unknown <eili_s@naver.com>
2022-07-21 23:06:39 +0900

    commit

---
+++ src/main/java/com/jh/TestService.java
@@ -0,0 +1,27 @@
+package com.jh;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+	public TestService() {
+		System.out.println("sssssssssssssssssss");
+		System.out.println("sssssssssssssssssss");
+		System.out.println("sssssssssssssssssss");
+		System.out.println("sssssssssssssssssss");
+		System.out.println("sssssssssssssssssss");
+		System.out.println("sssssssssssssssssss");
+	}
+	
+	
+	public void setPrintName(String name) {
+		System.out.println(name);
+		System.out.println(name);
+		System.out.println(name);
+		System.out.println(name);
+		System.out.println(name);
+		System.out.println(name);
+		
+	}
+
+}
---
+++ src/main/java/com/jh/www/HomeController.java
@@ -0,0 +1,53 @@
+package com.jh.www;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.jh.TestService;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	@Autowired
+	private TestService tesetService;
+	
+	
+	@Autowired
+	private ApplicationContext context;
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		System.out.println(tesetService);
+		TestService tt =  (TestService) context.getBean("xxx");
+		System.out.println(tt);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+}
